ce351ab7a789a6f0e996f19c7da1ee784751f4ad
frustratedswede <andrew.mossefin@gmail.com>
2017-08-16 15:23:04 -0600

    business intelligence
    built merchant favorite customer controller, route, and instance method in model. passing spec harness

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,2 +1,14 @@
 class Merchant < ApplicationRecord
+  has_many :invoices
+  has_many :customers, through: :invoices
+  has_many :transactions, through: :invoices
+
+  def favorite_customer
+    self.customers
+      .joins(:transactions)
+      .where(transactions: {result: "success"})
+      .group('customers.id')
+      .order('count(customers.id) DESC')
+      .first
+  end
 end
