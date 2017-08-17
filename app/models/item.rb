dac9c83cfd25bfde82ee6da61ddc78c3d1d59004
frustratedswede <andrew.mossefin@gmail.com>
2017-08-17 13:59:06 -0600

    fix bug
    changed mispelled 'merchant_id' in invoices controller.
    spec harness has 24 tests remaining.

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,10 @@ class Item < ApplicationRecord
   has_many :invoices, through: :invoice_items
 
   def self.most_revenue(quantity)
-    joins(:invoices).merge(Invoice.paid).group(:id).order('sum(invoice_items.quantity * invoice_items.unit_price) DESC')
+    joins(:invoices)
+      .merge(Invoice.paid)
+      .group(:id)
+      .order('sum(invoice_items.quantity * invoice_items.unit_price) DESC')
+      .limit(quantity)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 20170814223605) do
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.string "unit_price"
+    t.integer "unit_price"
     t.bigint "merchant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
